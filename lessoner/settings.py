09bc8b09ac89cf7407d7703cabd6da36a69e1b59
mykhailo-murat <mykhailo.murat@gmail.com>
2025-11-14 18:37:35 +0200

    init payment app. + controllers and serializers for subscription app

--- lessoner/settings.py
+++ lessoner/settings.py
@@ -49,6 +49,8 @@ LOCAL_APPS = [
     'apps.accounts',
     'apps.main',
     'apps.comments',
+    'apps.subscribe',
+    'apps.payment',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + LOCAL_APPS
